f32762ac99897099c82bcce661feaa1dce22535d
Orpheon <alex.liebendorf@gmail.com>
2016-08-26 12:11:50 +0200

    Added missing header guard.

--- include/global_constants.h
+++ include/global_constants.h
@@ -1,5 +1,9 @@
+#ifndef GLOBAL_CONSTANTS_H
+#define GLOBAL_CONSTANTS_H
 
 const double PHYSICS_FPS = 60.0;
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
+
+#endif // GLOBAL_CONSTANTS_H
