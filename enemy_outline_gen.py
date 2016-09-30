856d406e01efd3a3d316f77bd138538e0939899d
Orpheon <alex.liebendorf@gmail.com>
2016-09-30 13:47:04 +0200

    Made the enemy outline generator draw all around the sprite.

--- enemy_outline_gen.py
+++ enemy_outline_gen.py
@@ -5,26 +5,26 @@ import sys
 orig_im = Image.open(sys.argv[1])
 orig_im.convert("RGBA")
 w, h = orig_im.size
-scaled_im = Image.new("RGBA", (w+2, h+1))
+scaled_im = Image.new("RGBA", (w+2, h+2))
 scaled_im.paste(orig_im, (1, 1))
 
 scaled_pixels = scaled_im.load()
-im = Image.new("RGBA", (w+2, h+1))
+im = Image.new("RGBA", (w+2, h+2))
 pixels = im.load()
 
 for x in range(0, w+2):
-    for y in range(0, h+1):
+    for y in range(0, h+2):
         # If pixel is transparent
         if scaled_pixels[x, y][3] == 0:
             isborder = False
             for i, j in ([-1, 0], [1, 0], [0, -1], [0, 1]):
-                if scaled_pixels[max(0, min(w+1, x+i)), max(0, min(h, y+j))][3] != 0:
+                if scaled_pixels[max(0, min(w+1, x+i)), max(0, min(h+1, y+j))][3] != 0:
                     isborder = True
                     break
             if isborder:
                 # Make red
                 pixels[x, y] = (225, 17, 17)
         else:
-            pixels[x, y] = scaled_pixels[x, y]
+            pixels[x, y] = (0, 0, 0, 0)
 
 im.save(sys.argv[1].replace(".", "_enemy."))
