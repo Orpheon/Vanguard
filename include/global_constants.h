ec198c23cb84d9e39b9022ff1c7d8a8cb511d087
Orpheon <alex.liebendorf@gmail.com>
2016-10-27 18:24:04 +0200

    Enlarged window view a bit.

--- include/global_constants.h
+++ include/global_constants.h
@@ -5,7 +5,7 @@ const double ENGINE_TIMESTEP = 1.0/60.0;
 const double MENU_TIMESTEP = 1.0/60.0;
 const double NETWORKING_TIMESTEP = 1/20.0;
 
-const unsigned int VIEWPORT_WIDTH = 800;
+const unsigned int VIEWPORT_WIDTH = 1000;
 
 const int STAIRCASE_STEPSIZE = -6;
 
