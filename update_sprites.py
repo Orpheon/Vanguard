b9b94d075e66a37e7661a8d240cba7a00ace310b
Orpheon <alex.liebendorf@gmail.com>
2017-02-04 01:40:02 +0100

    Updated and remerged a better sprite updating script.

--- update_sprites.py
+++ update_sprites.py
@@ -3,19 +3,26 @@ from __future__ import print_function
 from PIL import Image
 import os
 import json
+import shutil
+
+# Delete the old sprites folder if it exists
+if os.path.exists("sprites"):
+    shutil.rmtree("sprites")
 
 filelist = []
-output = {}
+spritedata = {}
+# Get a list of all the sprites we're handling
 for (dirpath, dirnames, filenames) in os.walk("sprites template"):
-    if filenames != []:
-        output[dirpath.partition(os.path.sep)[2]+"/ number of frames"] = len(filenames)
+    nframes = 0
     for filename in filenames:
-        if filename.endswith('.png'):
+        if filename.endswith('_sprite.png'):
             filelist.append(os.sep.join([dirpath, filename]))
-
+            nframes += 1
+    spritedata[dirpath.partition(os.path.sep)[2]+"/ number of frames"] = nframes
 
 
 for filepath in filelist:
+    # Crop sprite to remove whitespace around and remember how much we cropped it by for rendering offset
     im = Image.open(filepath)
     im.convert(mode="RGBA")
     w, h = im.size
@@ -37,7 +44,7 @@ for filepath in filelist:
                 if b < y:
                     b = y
 
-    output[filepath.partition(os.path.sep)[2]] = [w/2-l, h/2-t]
+    spritedata[filepath.partition(os.path.sep)[2]] = [w/2-l, h/2-t]
     im2 = im.crop((l, t, r+1, b+1))
 
     outputfile = filepath.replace("sprites template", "sprites")
@@ -45,5 +52,33 @@ for filepath in filelist:
         os.makedirs(os.path.dirname(outputfile))
     im2.save(outputfile)
 
+    # Generate outline file if we're not in the ui folder
+    if os.path.sep+"ui"+os.path.sep not in filepath:
+        # Outlines are preferably generated from hitmask sprites if one exists
+        if os.path.exists(filepath.replace("_sprite.png", "_hitmask.png")):
+            im = Image.open(filepath.replace("_sprite.png", "_hitmask.png"))
+        else:
+            im = Image.open(filepath)
+
+        im.convert(mode="RGBA")
+        w, h = im.size
+        outline = Image.new("RGBA", im.size)
+        inputpix = im.load()
+        outlinepix = outline.load()
+        for x in range(w):
+            for y in range(h):
+                if inputpix[x, y][3] != 0:
+                    try:
+                        for i,j in ([-1, 0], [1, 0], [0, -1], [0, 1]):
+                            if inputpix[x+i, y+j][3] == 0:
+                                outlinepix[x, y] = (255, 255, 255, 177)
+                    except IndexError:
+                        # We're at the edge of the sprite image, that also counts as an edge of the sprite
+                        outlinepix[x, y] = (255, 255, 255, 177)
+
+        outline.crop((l, t, r+1, b+1)).save(filepath.replace("_sprite.png", "_outline.png").replace("sprites template", "sprites"))
+
+
+# Export the cropping data and the animation lengths
 with open("sprites/spritedata.json", "w") as f:
-    json.dump(output, f)
+    json.dump(spritedata, f)
