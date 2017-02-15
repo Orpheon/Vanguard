dbde96db6f5784e0ed4c4a59584ffd127518fb00
Orpheon <alex.liebendorf@gmail.com>
2017-02-15 21:05:48 +0100

    Since all entity creation will happen through gamestate, no need to test it all the time as it should only go through one method anyway.

--- include/entity.h
+++ include/entity.h
@@ -17,10 +17,10 @@ class Entity
     public:
         virtual ~Entity() = default;
         virtual void init(uint64_t id_);
-        virtual void beginstep(Gamestate *state, double frametime);
-        virtual void midstep(Gamestate *state, double frametime);
-        virtual void endstep(Gamestate *state, double frametime);
-        virtual void render(Renderer *renderer, Gamestate *state);
+        virtual void beginstep(Gamestate *state, double frametime) = 0;
+        virtual void midstep(Gamestate *state, double frametime) = 0;
+        virtual void endstep(Gamestate *state, double frametime) = 0;
+        virtual void render(Renderer *renderer, Gamestate *state) = 0;
         virtual bool isrootobject() = 0;
         virtual std::unique_ptr<Entity> clone() = 0;
         virtual void interpolate(Entity *prev_entity, Entity *next_entity, double alpha) = 0;
--- src/entity.cpp
+++ src/entity.cpp
@@ -7,39 +7,3 @@ void Entity::init(uint64_t id_)
     inited = true;
     id = id_;
 }
-
-void Entity::beginstep(Gamestate *state, double frametime)
-{
-    if (not inited)
-    {
-        std::cout << "Fatal error: Entity of type " << static_cast<int>(entitytype) << " beginstep was called before init function!";
-        throw -1;
-    }
-}
-
-void Entity::midstep(Gamestate *state, double frametime)
-{
-    if (not inited)
-    {
-        std::cout << "Fatal error: Entity of type " << static_cast<int>(entitytype) << " midstep was called before init function!";
-        throw -1;
-    }
-}
-
-void Entity::endstep(Gamestate *state, double frametime)
-{
-    if (not inited)
-    {
-        std::cout << "Fatal error: Entity of type " << static_cast<int>(entitytype) << " endstep was called before init function!";
-        throw -1;
-    }
-}
-
-void Entity::render(Renderer *renderer, Gamestate *state)
-{
-    if (not inited)
-    {
-        std::cout << "Fatal error: Entity of type " << static_cast<int>(entitytype) << " render was called before init function!";
-        throw -1;
-    }
-}
