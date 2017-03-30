fa81183cce513a1e8adef07faf9b1ea79374a9fb
Orpheon <alex.liebendorf@gmail.com>
2017-03-30 23:55:20 +0200

    Added shield to cmake/cbp and made its methods virtual.

--- include/ingameelements/shield.h
+++ include/ingameelements/shield.h
@@ -7,11 +7,11 @@ class Shield : MovingEntity
     public:
         virtual void init(uint64_t id_, Gamestate *state, std::string herofolder, bool isflipped_);
         virtual ~Shield() override = default;
-        void beginstep(Gamestate *state, double frametime) override;
-        void midstep(Gamestate *state, double frametime) override;
-        void endstep(Gamestate *state, double frametime) override;
-        void render(Renderer *renderer, Gamestate *state) override;
-        bool isrootobject() override {return false;};
-        std::unique_ptr<Entity> clone() override {return std::unique_ptr<Entity>(new Shield(*this));}
-        void interpolate(Entity *prev_entity, Entity *next_entity, double alpha) override;
+        virtual void beginstep(Gamestate *state, double frametime) override;
+        virtual void midstep(Gamestate *state, double frametime) override;
+        virtual void endstep(Gamestate *state, double frametime) override;
+        virtual void render(Renderer *renderer, Gamestate *state) override;
+        virtual bool isrootobject() override {return false;};
+        virtual void interpolate(Entity *prev_entity, Entity *next_entity, double alpha) override;
+        virtual bool collides(Gamestate *state, double x_, double y_) override;
 };
