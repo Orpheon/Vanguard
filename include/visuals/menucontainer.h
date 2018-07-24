9c31af3dc3cbe4008d23ff966c4f24918b9c1bbd
Orpheon <alex.liebendorf@gmail.com>
2018-07-24 18:56:01 +0200

    Ported menucontainer.

--- include/visuals/menucontainer.h
+++ include/visuals/menucontainer.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "allegro5/allegro5.h"
 #include "datastructures.h"
 
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -22,7 +21,6 @@ class MenuContainer
         int serverport;
 
     private:
-        ALLEGRO_EVENT_QUEUE *event_queue;
         std::unique_ptr<Menu> current_menu;
         bool finished;
 };
--- src/visuals/menucontainer.cpp
+++ src/visuals/menucontainer.cpp
@@ -7,44 +7,32 @@
 
 #include "global.h"
 
-MenuContainer::MenuContainer(ALLEGRO_DISPLAY *display) : planned_action(NOACTION), finished(false)
+MenuContainer::MenuContainer(sf::RenderWindow &window) : planned_action(NOACTION), finished(false)
 {
-    // Create an event queue, and error if it fails
-    event_queue = al_create_event_queue();
-    if (!event_queue)
-    {
-        Global::logging().panic(__FILE__, __LINE__, "Could not create event queue");
-    }
-
-    // Connect the window, keyboard and mouse events to this event queue
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-    al_register_event_source(event_queue, al_get_mouse_event_source());
-
-    current_menu = std::unique_ptr<Mainmenu>(new Mainmenu(display, *this));
+    current_menu = std::unique_ptr<Mainmenu>(new Mainmenu(window, *this));
 }
 
 MenuContainer::~MenuContainer()
 {
-    al_destroy_event_queue(event_queue);
+
 }
 
-bool MenuContainer::run(ALLEGRO_DISPLAY *display)
+bool MenuContainer::run(sf::RenderWindow &window)
 {
-    current_menu->run(display, event_queue);
+    current_menu->run(window);
     if (action() == POSTMENUACTION::OPEN_LOBBY)
     {
-        current_menu = std::unique_ptr<Menu>(new Lobbymenu(display, *this));
+        current_menu = std::unique_ptr<Menu>(new Lobbymenu(window, *this));
         planned_action = POSTMENUACTION::NOACTION;
     }
     else if (action() == POSTMENUACTION::OPEN_MAINMENU)
     {
-        current_menu = std::unique_ptr<Mainmenu>(new Mainmenu(display, *this));
+        current_menu = std::unique_ptr<Mainmenu>(new Mainmenu(window, *this));
         planned_action = POSTMENUACTION::NOACTION;
     }
     else if (action() == POSTMENUACTION::OPEN_OPTIONS)
     {
-        current_menu = std::unique_ptr<Optionsmenu>(new Optionsmenu(display, *this));
+        current_menu = std::unique_ptr<Optionsmenu>(new Optionsmenu(window, *this));
         planned_action = POSTMENUACTION::NOACTION;
     }
 
