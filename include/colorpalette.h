1ad6f03d60ddccaa1801f0c8e81fa23c5d8aea12
Orpheon <alex.liebendorf@gmail.com>
2018-08-02 15:28:09 -0400

    Fixed color palette to be static const.

--- include/colorpalette.h
+++ include/colorpalette.h
@@ -5,17 +5,17 @@
 // 3 arguments: R, G, B
 // 4 arguments: R, G, B, A
 
-sf::Color COLOR_HP(225, 225, 225);
-sf::Color COLOR_ARMOR(237, 223, 132);
-sf::Color COLOR_SHIELD(101, 206, 240);
-sf::Color COLOR_TORBARMOR(242, 197, 84);
-sf::Color COLOR_LUCIOSHIELD(69, 122, 255);
+static const sf::Color COLOR_HP(225, 225, 225);
+static const sf::Color COLOR_ARMOR(237, 223, 132);
+static const sf::Color COLOR_SHIELD(101, 206, 240);
+static const sf::Color COLOR_TORBARMOR(242, 197, 84);
+static const sf::Color COLOR_LUCIOSHIELD(69, 122, 255);
 
-sf::Color COLOR_ALLY(0, 0, 255);
-sf::Color COLOR_ENEMY(255, 0, 0);
-sf::Color COLOR_ENEMY_OUTLINE(255, 17, 17);
-sf::Color COLOR_SPAWNROOM_FILL(248, 222, 0, 10);
-sf::Color COLOR_SPAWNROOM_OUTLINE(248, 222, 0, 200);
-sf::Color COLOR_CP(248, 222, 0);
-sf::Color COLOR_MCCREE_LOCKON(253, 58, 58);
-sf::Color COLOR_MCCREE_TRAIL(133, 238, 238, 150);
+static const sf::Color COLOR_ALLY(0, 0, 255);
+static const sf::Color COLOR_ENEMY(255, 0, 0);
+static const sf::Color COLOR_ENEMY_OUTLINE(255, 17, 17);
+static const sf::Color COLOR_SPAWNROOM_FILL(248, 222, 0, 10);
+static const sf::Color COLOR_SPAWNROOM_OUTLINE(248, 222, 0, 200);
+static const sf::Color COLOR_CP(248, 222, 0);
+static const sf::Color COLOR_MCCREE_LOCKON(253, 58, 58);
+static const sf::Color COLOR_MCCREE_TRAIL(133, 238, 238, 150);
